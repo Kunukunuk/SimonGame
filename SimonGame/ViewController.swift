862409522be1ea7cd7985619d150f7a67e4a6485
Kun Huang <kkun.huang@gmail.com>
2018-03-22 12:04:35 -0400

    Initial Commit

---
+++ SimonGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonGame
+//
+//  Created by Kun Huang on 3/22/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
